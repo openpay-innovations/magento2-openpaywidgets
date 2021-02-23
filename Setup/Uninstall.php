beaa8fac98085951d6d0aec386399665eba0a760
Babar Ali <babara@openpay.com.au>
2021-02-23 18:25:38 +1100

    added the uninstallation script

---
+++ Setup/Uninstall.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Openpay\Widgets\Setup;
+
+use Magento\Framework\Setup\UninstallInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+
+class Uninstall implements UninstallInterface
+{
+    /**
+     * Module uninstall code
+     *
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function uninstall(
+        SchemaSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $setup->startSetup();
+        $connection = $setup->getConnection();
+        $setup->endSetup();
+    }
+}
