db6b217514fbcfdcc0b47b254a2dc21ea2c05494
babar-openpay <58763572+babar-openpay@users.noreply.github.com>
2021-12-16 16:12:48 +1100

    Removed the openpay logo from the order confirmation page (#1)

--- Plugin/Payment/Openpay.php
+++ Plugin/Payment/Openpay.php
@@ -1,29 +1,32 @@
 <?php
+
 namespace Openpay\Widgets\Plugin\Payment;
 
-class Openpay
-{
-	protected $helper;
-	
-	/*
-	 *@var \Magento\Framework\App\State
-	*/
-	protected $_state;
+class Openpay {
 
+    protected $helper;
 
-	public function __construct(
-		\Openpay\Widgets\Helper\Data $helper,
-		\Magento\Framework\App\State $state
-	) {
-		$this->helper = $helper;
-		$this->_state = $state;
-	}
+    /*
+     * @var \Magento\Framework\App\State
+     */
+    protected $_state;
+    protected $request;
 
-	public function afterGetTitle(\Openpay\Payment\Model\Openpay $subject, $result){
-		if ($this->helper->isCheckoutWidgetEnabled() && $this->_state->getAreaCode()!== 'adminhtml') {
-			$result .= ' <img src="https://static.openpay.com.au/brand/logo/amber-lozenge-logo.svg" width="75px" class="payment-icon opy-chk-logo"/> <span class="learn-more"><opy-learn-more-button></opy-learn-more-button></span> ';
-		}
-		return $result;
-	}
-	
+    public function __construct(
+            \Openpay\Widgets\Helper\Data $helper,
+            \Magento\Framework\App\State $state,
+            \Magento\Framework\App\RequestInterface $request
+    ) {
+        $this->helper = $helper;
+        $this->_state = $state;
+        $this->request = $request;
+    }
+
+    public function afterGetTitle(\Openpay\Payment\Model\Openpay $subject, $result) {
+        if ($this->helper->isCheckoutWidgetEnabled() && $this->_state->getAreaCode() !== 'adminhtml' && ($this->request->getFullActionName() !== 'checkout_onepage_success')) {
+            $result .= ' <img src="https://static.openpay.com.au/brand/logo/amber-lozenge-logo.svg" width="75px" class="payment-icon opy-chk-logo"/> <span class="learn-more"><opy-learn-more-button></opy-learn-more-button></span> ';
+        }
+        return $result;
+    }
+
 }
